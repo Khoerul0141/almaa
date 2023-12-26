98c593dd524ca3f44ee9f62899d544790bc5a2a2
rulls <68646796+Khoerul0141@users.noreply.github.com>
2023-12-27 04:52:08 +0700

    Update java alma.js

--- java alma.js
+++ java alma.js
@@ -1,6 +1,7 @@
 var html =
-  '<h2>Pesan Berhasil Terkirim</h2><h3>Anda akan diarahkan ke halaman utama</h3><h4 class="timer">10</h4>'
+  '<h2>Pesan Berhasil Terkirim</h2><h3>Anda akan diarahkan ke halaman utama</h3><h4 class="timer">10</h4>';
 
+document.querySelector("body").innerHTML += html;
 timer = document.querySelector(".timer");
 var time = timer.innerHTML;
 
